817b56ea57c8fe0f4970576553364543092c8c28
Mochi <mochiko@mochi.com>
2020-10-16 22:23:27 -0400

    get stores

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from calculate_distance import get_store_locations
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Hello'
+    longit='30.386163'
+    lat='-82.288778'
+    return get_store_locations(longit,lat)
 
 
 if __name__ == '__main__':
