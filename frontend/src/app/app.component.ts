71b8eb71af9a93765480de744e4b2938e2283758
Richard Huang <huangrichard2001@gmail.com>
2020-10-17 23:06:15 -0700

    Created map instantiation function

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { FormBuilder } from '@angular/forms';
 import { Validators} from '@angular/forms';
 
 import { from } from 'rxjs'
+import {Loader} from 'google-maps';
+import dotenv
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -67,4 +70,24 @@ export class AppComponent {
     }
 
   }
+
+  generateMap(res) {
+    // Just make sure the res output looks right
+    const user = res['user_geolocation'];
+    const store = res['store'].geolocation;
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: user,
+    });
+    const marker = new google.maps.Marker({
+      position: user,
+      map: map,
+    });
+    const best_store = new google.maps.Marker({
+      position: store,
+      map: map,
+    });
+  }
 }
+
+
