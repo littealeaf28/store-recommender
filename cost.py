56a1b6bae94a9c4e2b08cb78128be82f473e5a3a
Mochi <mochiko@mochi.com>
2020-10-17 21:45:42 -0400

    frontend directory

--- cost.py
+++ cost.py
@@ -1,5 +1,17 @@
 def get_cost(store_infos):
     print(store_infos)
     #print(type stores_info)
+    #distance[filter it ]
+    #cost
+    #inventory
+    #business
+    # for store in store_infos:
+    #     #calculate cost
+    #     cost()
+
+    #sort by cost [most optimal...least optimal] 
     return "An optimal store, or some stats about the optimality of each store\
                to be interpreted later? "
+
+def cost(distance,item_cost,is_inventory_present,business):
+    #calculate cost for each store
